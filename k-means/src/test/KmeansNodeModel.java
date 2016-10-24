7389682c30463d5b6b30b8cf8cd0d427a907aa92
Daniel <Daniel@Daniels-MacBook-Pro-2.local>
2016-10-24 11:32:42 +0200

    corrected typo

--- k-means/src/test/KmeansNodeModel.java
+++ k-means/src/test/KmeansNodeModel.java
@@ -128,7 +128,7 @@ public class KmeansNodeModel extends NodeModel {
 
 		BufferedDataContainer container = exec.createDataContainer(outputSpec);
 
-		// create 2. output table
+		// create second output table
 		DataColumnSpec[] allColSpecs2 = new DataColumnSpec[inDataSize + 1];
 		for (int i = 0; i < inDataSize; i++) {
 			allColSpecs2[i] = new DataColumnSpecCreator(columnNames[i], DoubleCell.TYPE).createSpec();
