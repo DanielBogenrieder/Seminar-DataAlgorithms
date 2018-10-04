e41d8c6d05bbf0882b919c4a43d0550bd97f8a21
DanielBogenrieder <elchman@outlook.de>
2018-10-04 15:16:24 +0200

    added author tag

--- k-means/src/test/KmeansNodePlugin.java
+++ k-means/src/test/KmeansNodePlugin.java
@@ -14,7 +14,7 @@ import org.osgi.framework.BundleContext;
  * If you want to move/rename this file, make sure to change the plugin.xml
  * file in the project root directory accordingly.
  *
- * @author 
+ * @author Georg Peter
  */
 public class KmeansNodePlugin extends Plugin {
     // The shared instance.
